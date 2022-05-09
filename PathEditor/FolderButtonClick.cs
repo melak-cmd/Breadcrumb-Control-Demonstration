ffd3fc028838988f0ddb88e0cef6e4b2104f7fe1
Amine <amine.kaouani@outlook.com>
2022-05-09 08:26:36 +0200

    struc

---
+++ PathEditor/FolderButtonClick.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace PathEditor
+{
+    public class FolderButtonClick : ICommand
+    {
+        private BreadcrumbControl owner;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public FolderButtonClick(BreadcrumbControl ow)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Shell/MainWindow.xaml.cs
+++ Shell/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BreadcrumbControlDemonstration
+namespace Shell
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
